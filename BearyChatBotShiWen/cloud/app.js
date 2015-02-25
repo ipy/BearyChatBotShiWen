8aea1d21df75f4ab39f7914b3214b84bb599d19f
Sin <dev@zouxin.net>
2015-02-25 06:42:56 +0000

    remove unecessary files

--- BearyChatBotShiWen/cloud/app.js
+++ BearyChatBotShiWen/cloud/app.js
@@ -4,14 +4,9 @@ var app = express();
 var shiwen = require('cloud/routes/shiwen');
 
 // App 全局配置
-app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
-// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
-app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
-});
 
 app.get('/', shiwen);
 app.post('/', shiwen);
